5128c356d5a8ba6340e26be30c1132b9d5d46398
Ankita Nema <Ankita.Nema1@T-Mobile.com>
2018-09-22 18:12:10 +0530

    First Commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Ingredient } from './../shared/ingredients.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[]=[
+    new Ingredient('Apple', 5),
+    new Ingredient('Mango', 10)
+
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
